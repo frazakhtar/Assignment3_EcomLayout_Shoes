ea614beb1ab64477964fec36b2f984bc3fe18717
frazakhtar <87436611+frazakhtar@users.noreply.github.com>
2025-09-09 20:24:46 +0530

    add material ui and Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { AppBar, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography, Box } from "@mui/material";
+
+const NavBar = () => {
+const navItems = ['Home', 'About', 'Contact'];
+  return (
+    <AppBar style={{ backgroundColor: "#154D71" }}>
+      <Toolbar>
+        <Typography>Logo</Typography>
+         <Box sx={{ ml: "auto" }} />
+        <List sx={{display:"flex"}}>
+          {navItems.map((item) => (
+            <ListItem key={item} disablePadding>
+              <ListItemButton sx={{ textAlign: "center" }}>
+                <ListItemText primary={item} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
